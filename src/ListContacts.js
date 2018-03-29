f2e879374d6c3c1b675899591ccd8df3a5d76abd
George Kemp <georgekemp94@gmail.com>
2018-03-29 23:06:01 +0100

    Lesson 3

---
+++ src/ListContacts.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    }
+    state = {
+        query: ''
+    }
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
+    render() {
+        const { query } = this.state;
+        const { contacts, onDeleteContact } = this.props;
+
+        const showingContacts = query === ""
+            ? contacts
+            : contacts.filter((c) => (
+                c.name.toLowerCase().includes(query.toLowerCase())
+            ))
+
+        return (
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                <input 
+                    className='search-contacts'
+                    type='text'
+                    placeholder='Search Contacts'
+                    value={query}
+                    onChange={(event)=>this.updateQuery(event.target.value)}
+                />
+                </div>
+            {showingContacts.length !== contacts.length && (
+                <div className='showing-contacts'>
+                    <span> Now showing {showingContacts.length} of { contacts.length } </span>
+                        <button onClick={()=>this.updateQuery("")}>Show All</button>
+                </div>
+            )}
+            <ol className='contacts-list'>
+            {showingContacts.map(contact => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div 
+                        className="contact-avatar"
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button 
+                        className='contact-remove contact-remove'
+                        onClick={() => onDeleteContact(contact)}
+                        >
+                        Remove 
+                    </button>
+                </li>
+            ))}
+            </ol>
+            </div>
+        )
+    }
+}
+
+
+
+export default ListContacts
